d1c81f73863bc41dd1564fa230f361d29a6d4fa0
beaavelino <beatrizavelino0088@gmail.com>
2021-12-16 16:51:46 -0300

    Finalizando a parte de Habilidades

--- static/script.js
+++ static/script.js
@@ -6,6 +6,19 @@ const body = document.querySelector('body');
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
     ? menuMobile.classList.replace("bi-list", "bi-x")
-    : menuMobile.classList.replace("bi-x", "bi-list")
-    body.classList.toggle("menu-nav-active")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
 });
+
+/*Fechar o menu quando clicar em algum item e mudar o icone para list */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
